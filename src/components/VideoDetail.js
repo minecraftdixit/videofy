b3b16b9bcbf0ef27a56aac2d73a0843ce661456a
minecraftdixit <63745645+minecraftdixit@users.noreply.github.com>
2020-09-26 16:11:48 +0530

    Update VideoDetail.js

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -11,7 +11,7 @@ const VideoDetail =({video})=>{
     return (
         <div>
             <div className="ui embed" >
-                <iframe title=" video title" src={videoSrc} />
+                <embed title=" video title" src={videoSrc} />
             </div>
          <div className="ui segment">
          <h4 className="ui header">{video.snippet.title} </h4>
