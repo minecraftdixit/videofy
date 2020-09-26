c52d07fedd7c62319ffe66b1f72123f6df2d4bd5
minecraftdixit <63745645+minecraftdixit@users.noreply.github.com>
2020-09-26 14:58:59 +0530

    Add files via upload

---
+++ src/components/App.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import SearchBar from './SearchBar' ;
+import youtube from '../apis/youtube';
+import VideoList from './VideoList';
+import VideoDetail from './VideoDetail' ;
+import DarkMode from './DarkMode';
+const dark= {
+  color: "white",
+  backgroundColor: "black",
+  padding: "10px",
+  height: "100vh"
+
+};
+
+class App extends React.Component { 
+    state = { videos :[] , selectedVideo: null } ;  
+     dark=(e)=>{
+       console.log("this isjkl");
+     }
+    onTermSubmit=async(term)=>{
+        const response = await youtube.get('/search',{
+          params:{
+               q : term
+          }  
+        });
+      this.setState({ videos : response.data.items})  ;
+
+    };
+    onVideoSelect =(video) => {
+        this.setState({ selectedVideo: video}) ;
+
+    };
+    render() { 
+        return (
+           
+        <div className="ui container"> 
+        <DarkMode onClick={this.dark}>dark mode</DarkMode>
+       
+        <SearchBar onFormSubmit={this.onTermSubmit}/>
+        <div className ="ui grid">
+            <div className ="ui row">
+           <div className="eleven wide column">     
+        <VideoDetail video={this.state.selectedVideo} />
+           </div>
+           <div className =" five wide column">
+        <VideoList 
+         onVideoSelect={ this.onVideoSelect } 
+            videos={this.state.videos}
+            />
+            </div>
+          </div>
+          </div>
+          </div>
+        );
+    }
+}
+ 
+export default App;
